c356d8b278bff72dc6c045bc607d967897741fa6
BrunoEOtavio <125394500+BrunoEOtavio@users.noreply.github.com>
2023-02-14 15:10:36 -0300

    Aula 03 Variaveis Burno e Otavio

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let VariavelA = 0
+let VariavelB = 0
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        VariavelA += 1
+        basic.showString("A=" + VariavelA)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        VariavelB += 2
+        basic.showString("B=" + VariavelB)
+    }
 })
